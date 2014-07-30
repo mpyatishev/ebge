298a2e993e8d5bb0cf3c8761300527b4681d1399
Maxim Pyatishev <m.pyatishev@dlinksys.ru>
2014-07-30 11:23:55 +0400

    + setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+setup(name="PyEBGE",
+      version='0.0.1',
+      description='Entity-Based Game Engine',
+      author='mpyatishev',
+      author_email='mpyatishev@gmail.com',
+      packages=['pyebge'],
+      install_requires=[],
+      license="MIT",
+      long_description='https://github.com/mpyatishev/ebge',
+      classifiers=[
+          "Intended Audience :: Developers",
+          "Development Status :: 5 - Production/Development",
+          "License :: OSI Approved :: MIT",
+          "Programming Language :: Python :: 3.3",
+          "Programming Language :: Python :: 3.4",
+          "Topic :: Games/Entertainment",
+      ],
+)
